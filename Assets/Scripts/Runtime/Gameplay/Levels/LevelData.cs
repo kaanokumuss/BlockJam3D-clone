bbaabd742d36645eea5a79b29809d85c889522d0
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-08-13 18:22:08 +0300

    Added PlanetData

---
+++ Assets/Scripts/Runtime/Gameplay/Levels/LevelData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct LevelData
+{
+    public string title;
+    public Planet[] planet;
+}
---
+++ Assets/Scripts/Runtime/Gameplay/Levels/PlanetData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlanetData
+{
+    public int id;
+    public Vector3 position;
+    public string planet;
+    public string path;
+    public int[] children;
+}
+
