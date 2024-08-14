282a6174ed08bcf4dfb21b1e1ca02d369f13bd9d
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-08-14 14:17:04 +0300

    LevelManager load method created

--- Assets/Scripts/Runtime/Managers/LevelManager.cs
+++ Assets/Scripts/Runtime/Managers/LevelManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,10 +7,12 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private TextAsset[] levelFiles;
     private LevelData[] _levelData;
+    private LevelSaveData _levelSaveData;
 
     private void Awake()
     {
         ReadLevels();
+        Load();
     }
 
     void ReadLevels()
@@ -18,6 +21,33 @@ public class LevelManager : MonoBehaviour
         for (int i = 0; i < levelFiles.Length; i++)
         {
             _levelData[i] = JsonUtility.FromJson<LevelData>(levelFiles[i].text);
+        }
+    }
+
+    void Load()
+    {
+        // todo: save edilen data var mı diye bak ,  level save datasını çek . 
+        if (DataHandler.HasData(DataKeys.LevelScoreDataKey)) // burada bir data var mi diye checkliyoruz. key tutmak gerekiyor bunun icin yeni sc actik 
+        { 
+            //yukaridaki level save datayi data handler daki load la json a aktariyoruz . içine LevelSaveData class ını veriyoruz. çünkü ordaki şeyleri kullanacağız . 
+            _levelSaveData = DataHandler.Load<LevelSaveData>(DataKeys.LevelScoreDataKey);
+        }
+        else // Oyuna ilk başladığımızda ya da data silindiğinde
+        {
+            _levelSaveData = new LevelSaveData(new LevelScoresData[_levelData.Length]); // yeni oluşturulan dataları setle
+            for (int i = 0; i<_levelData.Length; i++)
+            {
+                _levelSaveData.Data[i].index = i;
+                _levelSaveData.Data[i].title = _levelData[i].title;
+                _levelSaveData.Data[i].highScore = 0;
+                _levelSaveData.Data[i].isUnlocked = false;
+                
+            }
+
+            _levelSaveData.Data[0].isUnlocked = true;
+            _levelSaveData.Data[0].highScore = 0;
+            
+            DataHandler.Save(_levelSaveData, DataKeys.LevelScoreDataKey);
         }
     }
 
---
+++ Assets/Scripts/Runtime/Util/DataKeys.cs
@@ -0,0 +1,7 @@
+public static  class DataKeys
+{
+      // static ve constlarin farki constlar compile edilirken belirlenir . 
+      // staticler runtime da değişebilir . 
+      // const daha optime 
+      public const string LevelScoreDataKey = "LevelScoreDataKey";
+}
