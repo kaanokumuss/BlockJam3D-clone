60a15e366078ae49da9e7a9c982963a8f6f1fd15
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-08-20 11:45:23 +0300

    Update SubmitManager.cs

--- Assets/Scripts/Runtime/Managers/SubmitManager.cs
+++ Assets/Scripts/Runtime/Managers/SubmitManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
@@ -9,9 +10,11 @@ public class SubmitManager : MonoBehaviour
     [SerializeField] MatchManager matchManager;
     [SerializeField] public Transform[] submitPositions;
     [SerializeField] private Button undoButton;
-    public  List<Sphere> sphereInfos = new List<Sphere>();
+    public List<Sphere> sphereInfos = new List<Sphere>();
     public bool isCheckingForMatch = false;
+    public float delay = 1f;
     public Stack<Sphere> undoStack = new Stack<Sphere>(); 
+    private bool canTap = true; // Yeni değişken eklendi.
 
     void OnEnable()
     {
@@ -25,9 +28,10 @@ public class SubmitManager : MonoBehaviour
 
     void HandleElementTapped(ITouchable touchedElement)
     {
-        if (isCheckingForMatch) return;
+        if (isCheckingForMatch || !canTap) return; // `canTap` kontrolü eklendi.
 
         isCheckingForMatch = true;
+        canTap = false; // Dokunma işlemi başladıktan sonra dokunma devre dışı bırakılır.
 
         GameObject touchedSphere = touchedElement.gameObject;
         Renderer renderer = touchedSphere.GetComponent<Renderer>();
@@ -43,10 +47,18 @@ public class SubmitManager : MonoBehaviour
         {
             Debug.LogError("No available index found for the color.");
             isCheckingForMatch = false;
+            canTap = true; // Hata durumunda tekrar dokunma aktif hale getirilir.
         }
+
+        StartCoroutine(EnableTapAfterDelay(delay)); // 2 saniye bekleme başlatılır.
     }
 
-   
+    // Yeni Coroutine metodu eklendi.
+    private IEnumerator EnableTapAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        canTap = true;
+    }
 
     public void UpdateSphereInfo(GameObject sphere, int index, Material material)
     {
@@ -66,9 +78,4 @@ public class SubmitManager : MonoBehaviour
             Debug.LogError("Sphere component not found on the provided GameObject.");
         }
     }
-
-
-    
-
-   
 }
