3a46f3706281b91d1a6478e4ffa0bc25da0a1758
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-08-25 10:54:31 +0300

    remainingAttemps on UndoButton

--- Assets/Scripts/Runtime/Gameplay/UI/UndoButton.cs
+++ Assets/Scripts/Runtime/Gameplay/UI/UndoButton.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
+using TMPro; // TextMeshPro'yu kullanmak için
 
 public class UndoButton : MonoBehaviour
 {
     [SerializeField] private MatchManager matchManager;
     [SerializeField] private SubmitManager submitManager;
     [SerializeField] private Button undoButton;
+    [SerializeField] private TextMeshProUGUI remainingAttemptsText; // TMP referansı
+
+    private bool hasUndone = false;
+    [SerializeField] int remainingAttempts = 1; // Başlangıçta 1 hakkı var
 
     private void OnEnable()
     {
         undoButton.onClick.AddListener(OnClick);
+        UpdateRemainingAttemptsText(); // TMP metnini güncelle
     }
 
     private void OnDisable()
@@ -20,7 +25,13 @@ public class UndoButton : MonoBehaviour
 
     private void OnClick()
     {
-        UndoLastMove();     
+        if (!hasUndone && remainingAttempts > 0)
+        {
+            UndoLastMove();
+            hasUndone = true;
+            remainingAttempts--; // Kalan hakkı bir azalt
+            UpdateRemainingAttemptsText(); // TMP metnini güncelle
+        }
     }
 
     public void UndoLastMove()
@@ -39,27 +50,15 @@ public class UndoButton : MonoBehaviour
                 // Trigger score event
                 matchManager.RearrangeSpheres();
                 ScoreEvents.OnTappedUndoButton?.Invoke();
-                
             });
         }
-        // if (submitManager.undoStack.Count > 0)
-        // {
-        //     Sphere lastMovedSphere = submitManager.undoStack.Pop();
-        //     Debug.Log("Popped");
-        //
-        //     // Move the sphere back and pass a callback to handle completion
-        //     lastMovedSphere.MoveBack(() =>
-        //     {
-        //         // Remove sphere from info list
-        //         submitManager.sphereInfos.RemoveAll(info => info.SphereObject == lastMovedSphere.gameObject);
-        //         submitManager.isCheckingForMatch = false;
-        //
-        //         // Rearrange spheres after Undo
-        //         matchManager.RearrangeSpheres();
-        //
-        //         // Trigger score event
-        //         ScoreEvents.OnTappedUndoButton?.Invoke();
-        //     });
-        // }
+    }
+
+    private void UpdateRemainingAttemptsText()
+    {
+        if (remainingAttemptsText != null)
+        {
+            remainingAttemptsText.text = remainingAttempts.ToString();
+        }
     }
 }
